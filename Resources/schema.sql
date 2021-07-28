b4eb8edff1e8402d8989c307d9dc38b61f7b3b5e
Kyle Gee <geekyle4@gmail.com>
2021-07-27 17:00:32 -0700

    updating schema with tables for avaliable data

--- Resources/schema.sql
+++ Resources/schema.sql
@@ -1,61 +1,32 @@
-DROP TABLE min_wage;
--- Create min_wage Table
-CREATE TABLE min_wage (
+-- Creating ave_wage_indexing
+-- contains file average_wage_indexing_1951-2020.csv
+CREATE TABLE ave_wage_indexing (
 	"Year" INT,
-	"State" TEXT,
-	"State_Minimum_Wage" DECIMAL,
-	"State_Minimum_Wage_2020_Dollars" DECIMAL,
-	"Federal_Minimum_Wage" DECIMAL,
-	"Federal_Minimum_Wage_2020_Dollars" DECIMAL,
-	"Effective_Minimum_Wage" DECIMAL,
-	"Effective_Minimum_Wage_2020_Dollars" DECIMAL,
-	"CPI_Average" DECIMAL,
-	"Department_Of_Labor_Uncleaned_Data" VARCHAR,
-	"Department_Of_Labor_Cleaned_Low_Value" DECIMAL,
-	"Department_Of_Labor_Cleaned_Low_Value_2020_Dollars" VARCHAR,
-	"Department_Of_Labor_Cleaned_High_Value" VARCHAR,
-	"Department_Of_Labor_Cleaned_High_Value_2020_Dollars" VARCHAR,
-	"Footnote" VARCHAR
+	AWI VARCHAR,
+	Annual_change VARCHAR,
+	PRIMARY KEY ("Year")
 );
 
-select * from min_wage;
-
--- Creating unemployment_rate table
-CREATE TABLE unemp_rate (
-	unemployment_rate DECIMAL,
-	"state" VARCHAR,
-	"year" INT
-);
-
-SELECT * FROM unemp_rate;
-
-CREATE TABLE min_wage_effective (
-	"year" INT,
-	"state" VARCHAR,
-	minimum_wage_effective DECIMAL,
-	minimum_wage_2020_nominal DECIMAL,
-	CPI_Average DECIMAL
-);
-
-SELECT * FROM min_wage_effective;
-
--- Creating final dataset table
-
-CREATE TABLE economic_features (
-	"year" INT,
-	"state" VARCHAR,
-	population_million DECIMAL,
-	education_million DECIMAL,
-	welfare_million DECIMAL,
-	crime_rate DECIMAL,
-	unemployment_rate DECIMAL,
-	divorce_rate_per_1000_people DECIMAL,
-	homeownership_rate DECIMAL,
-	minimum_wage_effective DECIMAL,
+-- Creating table cpi_inflation_rate
+-- contains file CPI_inflation_rate_yearly_1914-2021_1984base.csv
+CREATE TABLE cpi_inflation_rate (
+	"Year" INT,
 	CPI_Average DECIMAL,
-	avg_wage_index DECIMAL,
-	poverty_rate DECIMAL
+	InflationRate VARCHAR,
+	PRIMARY KEY ("Year")
 );
+
+-- Creating table crime_rate
+-- Contains file crime_rate.csv
+CREATE TABLE crime_rate (
+	"year" INT,
+	"state" TEXT,
+	crime_rate DECIMAL,
+	PRIMARY KEY ("year", "state")
+);
+
+-- Creating final dataset table that contains all economic features
+-- Contains file dataset.csv
 
 CREATE TABLE economic_features_full (
 	"year" INT,
@@ -70,8 +41,84 @@ CREATE TABLE economic_features_full (
 	minimum_wage_effective DECIMAL,
 	CPI_Average DECIMAL,
 	avg_wage_index DECIMAL,
-	poverty_rate DECIMAL
+	poverty_rate DECIMAL,
+	PRIMARY KEY ("year", "state")
 );
 
-SELECT * FROM economic_features_full;
+-- Creating final dataset that has all economic features with NA values dropped
+-- Contains file dataset_na_dropped.csv
+CREATE TABLE economic_features (
+	"year" INT,
+	"state" VARCHAR,
+	population_million DECIMAL,
+	education_million DECIMAL,
+	welfare_million DECIMAL,
+	crime_rate DECIMAL,
+	unemployment_rate DECIMAL,
+	divorce_rate_per_1000_people DECIMAL,
+	homeownership_rate DECIMAL,
+	minimum_wage_effective DECIMAL,
+	CPI_Average DECIMAL,
+	avg_wage_index DECIMAL,
+	poverty_rate DECIMAL,
+	PRIMARY KEY ("year", "state")
+);
+
+-- Creating divorce_rate table
+-- Contains file divorce_rate_per_1000_people.csv
+CREATE TABLE divorce_rate (
+	"Year" INT,
+	Divorce_rate_per_1000_people DECIMAL,
+	PRIMARY KEY ("Year")
+);
+
+-- Creating table homeownership_rate
+-- Contains file homeownership_rate.csv
+CREATE TABLE homeownership_rate (
+	homeownership_rate DECIMAL,
+	"state" VARCHAR,
+	"year" INT,
+	PRIMARY KEY ("year", "state")
+);
+
+-- Creating table min_wage_effective
+-- Contains file miimum_wage_effective_1968-2020.csv
+CREATE TABLE min_wage_effective (
+	"year" INT,
+	"state" VARCHAR,
+	minimum_wage_effective DECIMAL,
+	minimum_wage_2020_nominal DECIMAL,
+	CPI_Average DECIMAL,
+	PRIMARY KEY ("year", "state")
+);
+
+-- Creating table poverty_rates
+-- Contains file poverty_rates_1967-2014.csv
+CREATE TABLE poverty_rates (
+	"year" INT,
+	poverty_rate DECIMAL,
+	"state" VARCHAR,
+	PRIMARY KEY ("year", "state")
+);
+
+-- Creating table unemployment_rate
+-- Contains file unemployment_rate.csv
+CREATE TABLE unemployment_rate (
+	unemployment_rate DECIMAL,
+	"state" VARCHAR,
+	"year" INT,
+	PRIMARY KEY ("year", "state")
+);
+
+-- Creating table welfare_education
+-- Contains file welfare_education_1970-2026.csv
+CREATE TABLE welfare_education (
+	"year" INT,
+	"state" VARCHAR,
+	population_million DECIMAL,
+	education_million DECIMAL,
+	welfare_million DECIMAL,
+	PRIMARY KEY ("year", "state")
+);
+
 
